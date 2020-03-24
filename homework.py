7dd94c8c006c80f49c0caca1cc6a6244d74c143f
unknown <aliyawrite@gmail.com>
2020-03-24 20:30:59 +0500

    III

--- homework.py
+++ homework.py
@@ -1,26 +1,30 @@
 import os
 import requests
-import telegram
 import time
+
+import telegram
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
-PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
+headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+params = {'from_date': 0}
+
 proxy = telegram.utils.request.Request(proxy_url='https://62.201.238.250:8080') 
-url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-params = {'from_date':0}
+bot = telegram.Bot(token=TELEGRAM_TOKEN, request=proxy)
 
 
 def parse_homework_status(homework):
-    response = requests.get(url=url, headers=headers, params=params).json().get('homeworks')[0]
-    homework_name = response['homework_name']
+    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+    response = requests.get(url=url, headers=headers, params=params).json().get('homeworks')
+    homework_name = response[0]['homework_name']
 
-    if response['status'] == 'rejected':
+    if response[0]['status'] == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
@@ -28,30 +32,33 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    homework_statuses = requests.get(url=url, headers=headers, params=params)
-    return homework_statuses.json()
+    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+    homework_statuses = requests.get(url=url, headers=headers, params=params).json()
+    return homework_statuses
 
 
-def send_message(message):
-    bot = telegram.Bot(token=TELEGRAM_TOKEN, request=proxy)
+def send_message(message, bot):
     return bot.send_message(chat_id=CHAT_ID, text=message)
     
+
 def main():
     current_timestamp = int(time.time())  # начальное значение timestamp
 
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
-            if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
+            if new_homework:
+                send_message(parse_homework_status(new_homework.get('homeworks')), bot)
             current_timestamp = new_homework.get('current_date')  # обновить timestamp
             time.sleep(300)  # опрашивать раз в пять минут
 
         except Exception as e:
             print(f'Бот упал с ошибкой: {e}')
-            time.sleep(5)
-            continue
+            time.sleep(10)
 
+        except KeyboardInterrupt:
+            print('Бот остановлен.')
+            break
 
 if __name__ == '__main__':
     main()
